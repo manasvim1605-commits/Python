6aef2725595577e906fbfd1b019e3403114db669
manasvim1605-commits <manasvim1605@gmail.com>
2026-09-11 15:30:56 +0530

    Nine commit

---
+++ code9.py
@@ -0,0 +1,24 @@
+a=0
+b=1
+c=2
+d=3
+e=4
+f=5
+g=6
+val=int(input("Enter a value "))
+if(val==a):
+    print("Monday")
+elif(val==b):
+    print("Tuesday")
+elif(val==c):
+    print("Wednesday")
+elif(val==d):
+    print("Thursday")
+elif(val==e):
+    print("Friday")
+elif(val==f):
+    print("Saturday")
+elif(val==g):
+    print("Sunday")
+else:
+    print("ENTER FROM 0-6 cuz week has only 7 dayssss :)")
